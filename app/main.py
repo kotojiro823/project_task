6898b6891cfdfbe4d9792babdef6d65dd6a667e0
kotojiro823 <kt.hanazawa52106@@gmail.com>
2026-02-20 00:25:33 +0900

    Fix CORS: allow frontend origin

--- app/main.py
+++ app/main.py
@@ -13,9 +13,15 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 app = FastAPI()
 
+# 許可するオリジン（フロントエンドのURL） 
+origins = [ 
+    "http://localhost:3000", # ローカル開発用（Reactなど） 
+    "https://todo-frontend-mjuw.onrender.com", # 本番用（デプロイ先） 
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
